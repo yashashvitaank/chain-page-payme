d1af2f6ae5c5a298d8c62288c5258dade68302ea
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2024-09-19 01:56:35 +0530

    Block Description

---
+++ src/data/DataBlock.js
@@ -0,0 +1,23 @@
+// data.js
+export const blockData = {
+    blockHeight: "61982172",
+    timestamp: "Sep-18-2024 04:38:47 PM +UTC",
+    transactions: {
+      count: 72,
+      internalTransactions: 4
+    },
+    validatedBy: "0x67b94473d81d0cd00849d563c94d0432ac988b49",
+    blockReward: "1.259989324858876834 POL",
+    difficulty: 28,
+    totalDifficulty: 1164176734,
+    size: "39,097 bytes",
+    gasUsed: {
+      amount: "10,777,308",
+      percentage: "35.92%",
+      gasTarget: "-28% Gas Target",
+    },
+    gasLimit: "30,000,000",
+    baseFeePerGas: "31 wei (0.000000031 Gwei)",
+    burntFees: "0.000000000334096548 POL",
+  };
+  
